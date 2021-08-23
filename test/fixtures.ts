d948925793a7700c6e0c881f50c3a374d875a5f4
bakuchi- <helveticaf0015se@gmail.com>
2021-08-23 17:28:45 +0900

    test:fix fixtures

--- test/fixtures.ts
+++ test/fixtures.ts
@@ -2,15 +2,14 @@ import hre, { waffle, ethers } from "hardhat"
 import { Fixture } from "ethereum-waffle"
 import {
     ERC20Mock,
+    WETH,
     CTokenMock,
     ComptrollerMock,
     PriceOracleMock,
     PokeMe,
     PokeMeReady,
-
-    // UniswapV2Router02,
-    // UniswapV2Factory,
-    // UniswapV2Pair,
+    UniswapV2Router02Mock,
+    UniswapV2PairMock,
 } from "../typechain"
 
 export const creamFixture: Fixture<{
@@ -20,12 +19,29 @@ export const creamFixture: Fixture<{
     cToken1: CTokenMock
     comptroller: ComptrollerMock
     oracle: PriceOracleMock
+    router: UniswapV2Router02Mock
+    pair: UniswapV2PairMock
 }> = async () => {
-    const ERC20MockFactory = await ethers.getContractFactory("ERC20Mock")
+    const [
+        WEthFactory,
+        ERC20MockFactory,
+        CTokenMockFactory,
+        ComptrollerFactory,
+        OracleFactory,
+        RouterFactory,
+        PairFactory,
+    ] = await Promise.all([
+        ethers.getContractFactory("WETH"),
+        ethers.getContractFactory("ERC20Mock"),
+        ethers.getContractFactory("CTokenMock"),
+        ethers.getContractFactory("ComptrollerMock"),
+        ethers.getContractFactory("PriceOracleMock"),
+        ethers.getContractFactory("UniswapV2Router02Mock"),
+        ethers.getContractFactory("UniswapV2PairMock"),
+    ])
+    const weth = (await WEthFactory.deploy()) as WETH
     const token0 = (await ERC20MockFactory.deploy("Token0", "TOKEN0")) as ERC20Mock
     const token1 = (await ERC20MockFactory.deploy("Token1", "TOKEN1")) as ERC20Mock
-
-    const CTokenMockFactory = await ethers.getContractFactory("CTokenMock")
     const cToken0 = (await CTokenMockFactory.deploy(
         "cToken0",
         "CTOKEN0",
@@ -38,13 +54,13 @@ export const creamFixture: Fixture<{
         token1.address,
         ethers.constants.AddressZero,
     )) as CTokenMock
-
-    const ComptrollerFactory = await ethers.getContractFactory("ComptrollerMock")
-    const comptroller = (await ComptrollerFactory.deploy([cToken0.address, cToken1.address])) as ComptrollerMock
-
-    const Oracle = await ethers.getContractFactory("PriceOracleMock")
-    const oracle = (await Oracle.deploy()) as PriceOracleMock
-    return { token0, token1, cToken0, cToken1, comptroller, oracle }
+    const [comptroller, oracle, router, pair] = (await Promise.all([
+        ComptrollerFactory.deploy([cToken0.address, cToken1.address]),
+        OracleFactory.deploy(),
+        RouterFactory.deploy(weth.address),
+        PairFactory.deploy(),
+    ])) as [ComptrollerMock, PriceOracleMock, UniswapV2Router02Mock, UniswapV2PairMock]
+    return { token0, token1, cToken0, cToken1, comptroller, oracle, router, pair }
 }
 
 // export const gelatoFixture: Fixture<{
@@ -69,18 +85,17 @@ export const creamFixture: Fixture<{
 // const pairAddr = (await getEvents(factory, createPairTx)).find(e => e.name == "PairCreated").args[2];
 // pair = (await ethers.getContractAt("UniswapV2Pair", pairAddr)) as IUniswapV2Pair;
 
-// export const uniswapFixture: Fixture<{
+// export const uniswapMockFixture: Fixture<{
 //     token0: ERC20Mock
 //     token1: ERC20Mock
 //     cToken0: CTokenMock
 //     cToken1: CTokenMock
 //     comptroller: ComptrollerMock
 // }> = async () => {
-//     const ComptrollerFactory = await ethers.getContractFactory("ComptrollerMock")
-//     const comptroller = (await ComptrollerFactory.deploy([cToken0.address, cToken1.address])) as ComptrollerMock
+//     const UniswapV2Router02Mock = await ethers.getContractFactory("UniswapV2Router02Mock")
+//     const router = (await UniswapV2Router02Mock.deploy([cToken0.address, cToken1.address])) as UniswapV2Router02Mock
 
-//     await comptroller.setAssetsIn([cToken0.address, cToken1.address])
-//     return { token0, token1, cToken0, cToken1, comptroller }
+//     return { token0, token1, cToken0, cToken1, router }
 // }
 
 // // const addLiquidity = async (signer: Wallet, pair, token0: ERC20, token1: ERC20, amount = toWei("1")) => {
